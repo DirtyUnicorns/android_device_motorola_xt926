52a5ba24d163eb4b4a8546e866d1e5f837d7f149
jl90 <leonardjason90.jl@gmail.com>
2015-05-31 09:17:47 -0700

    Update full_xt926.mk

--- full_xt926.mk
+++ full_xt926.mk
@@ -30,7 +30,7 @@ $(call inherit-product, device/motorola/xt926/device.mk)
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := xt926
-PRODUCT_NAME := full_xt926
+PRODUCT_NAME := du_xt926
 PRODUCT_BRAND := motorola
 PRODUCT_MODEL := DROID RAZR HD
 PRODUCT_MANUFACTURER := motorola
